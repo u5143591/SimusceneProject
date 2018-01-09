7eaa87a7469f50e21f9307ad9a0287d27f024e2c
admin <admin@172.18.26.38>
2018-01-09 08:27:24 +0800

    the whole project

---
+++ SimScene/src/UserBean.java
@@ -0,0 +1,63 @@
+
+public class UserBean {
+	 	private String name;  
+	    private String password;  
+	    private String privilege;  
+	    private String vdbbase;  
+	    private String vdbdetail;
+	    private String sim;
+	    private String download;
+	    private String admin;
+	    
+	       
+	    public String getName() {  
+	        return name;  
+	    }  
+	    public void setName(String name) {  
+	        this.name = name;  
+	    }  
+	    public String getPsw() {  
+	        return password;  
+	    }  
+	    public void setPsw(String password) {  
+	        this.password = password;  
+	    }  
+	    public String getPrivilege() {  
+	        return privilege;  
+	    }  
+	    public void setPrivilege(String privilege) {  
+	        this.privilege = privilege;  
+	    }  
+	      
+	    public String getVdbbase() {  
+	        return vdbbase;  
+	    }  
+	    public void setVdbbase(String vdbbase) {  
+	        this.vdbbase = vdbbase;  
+	    }
+	    public String getVdbdetail() {  
+	        return vdbdetail;  
+	    }  
+	    public void setVdbdetail(String vdbdetail) {  
+	        this.vdbdetail = vdbdetail;  
+	    }  
+	    public String getSim() {  
+	        return sim;  
+	    }  
+	    public void setSim(String sim) {  
+	        this.sim = sim;  
+	    }
+	    public String getDownload() {  
+	        return download;  
+	    }  
+	    public void setDownload(String download) {  
+	        this.download = download;  
+	    }
+	    public String getAdmin() {  
+	        return admin;  
+	    }  
+	    public void setAdmin(String admin) {  
+	        this.admin = admin;  
+	    }  
+	    
+}
